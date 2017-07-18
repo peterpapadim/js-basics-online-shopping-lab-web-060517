1666eae4792aa9a8fe26d402c9e1ef8aa7fb0f89
Peter <ppapadim12@gmail.com>
2017-07-18 17:10:10 -0400

    Done.

--- index.js
+++ index.js
@@ -10,21 +10,64 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var price = Math.floor(Math.random()*100)
+  cart.push({[item]: price})
+  console.log(`${item} has been added to your cart.`)
+  return cart
 }
 
 function viewCart() {
-  // write your code here
-}
+  var itemsInCart = []
+
+  for (let i = 0; i < cart.length; i++){
+    itemsInCart.push(`${Object.keys(cart[i])[0]} at \$${Object.values(cart[i])[0]}`)
+  }
+
+  if (itemsInCart.length == 0) {
+    console.log("Your shopping cart is empty.")
+  }
+  else if (itemsInCart.length == 1){
+    console.log(`In your cart, you have ${itemsInCart.join()}.`)
+  }
+  else if (itemsInCart.length == 2){
+    console.log(`In your cart, you have ${itemsInCart.join(" and ")}.`)
+  }
+  else {
+    itemsInCart.splice(-1, 0, "and")
+    var lastItem = itemsInCart.pop()
+    console.log(`In your cart, you have ${itemsInCart.join(", ")} ${lastItem}.`)
+    }
+  }
 
 function total() {
-  // write your code here
+  var total = []
+  for (let i = 0; i < cart.length; i++){
+    total.push(Object.values(cart[i])[0])
+  }
+  var sum = 0
+  for (let i = 0; i < total.length; i++){
+    sum += total[i]
+  }
+  return sum
 }
 
 function removeFromCart(item) {
-  // write your code here
+  for (let i = 0; i < cart.length; i++){
+    if (item === (Object.keys(cart[i])[0]) ){
+      cart.splice(i, 1)
+      return cart
+    }
+  }
+  console.log("That item is not in your cart.")
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if (!cardNumber){
+    console.log(  "Sorry, we don't have a credit card on file for you.")
+  }
+  else {
+    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
+    cart = []
+  }
+
 }
